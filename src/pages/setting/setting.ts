a24168fbea84254f6e2acf26d1c4a09088a9b4c1
lichenxuan <1428120566@qq.com>
2018-06-05 20:24:21 +0800

    测试、设置

--- src/pages/setting/setting.ts
+++ src/pages/setting/setting.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {ActionSheetController, AlertController, IonicPage, NavController, NavParams} from 'ionic-angular';
 
 /**
  * Generated class for the SettingPage page.
@@ -15,11 +15,63 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SettingPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public actionSheetCtrl:ActionSheetController,
+              public alertCtrl:AlertController) {
   }
+
+  language:any='English';
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SettingPage');
+  }
+
+  presentActionSheet() {
+    console.log(11)
+    const actionSheet = this.actionSheetCtrl.create({
+      title: '语言',
+      buttons: [
+        {
+          text: 'English',
+          handler: () => {
+            this.language='English';
+          }
+        },{
+          text: '简体中文',
+          handler: () => {
+            this.language='简体中文';
+          }
+        },{
+          text: '日本语',
+          handler: () => {
+            this.language='日本语';
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  presentConfirm() {
+    let alert = this.alertCtrl.create({
+      message: 'Are you sure?',
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'OK',
+          handler: () => {
+            console.log('Ok clicked');
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 
 }
