825273c6a3e3f1605a3c9450e4ec58591b4c0bd3
wanglindong <158338402@qq.com>
2018-07-24 14:44:44 +0800

    看图识词增加音频

--- src/pages/look-pic-recog-word/look-pic-recog-word.ts
+++ src/pages/look-pic-recog-word/look-pic-recog-word.ts
@@ -15,35 +15,36 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LookPicRecogWordPage {
 
-  over:boolean=false;
-  score:any=0;
-  questions:any=[];
-  question:any={
-    picSrc:'',
-    option1:'',
-    option2:'',
-    option3:'',
-    option4:'',
-    selected:'',
-    meaning:''
+  over: boolean = false;
+  score: any = 0;
+  questions: any = [];
+  question: any = {
+    picSrc: '',
+    audioSrc: '',
+    option1: '',
+    option2: '',
+    option3: '',
+    option4: '',
+    selected: '',
+    meaning: ''
   };
-  selected:any="-";
-  selected1:boolean=false;
-  selected2:boolean=false;
-  selected3:boolean=false;
-  selected4:boolean=false;
-  index:any=0;
-  time:any=30;
-  timer:any;
-  yourAnswer:any;
+  selected: any = "-";
+  selected1: boolean = false;
+  selected2: boolean = false;
+  selected3: boolean = false;
+  selected4: boolean = false;
+  index: any = 0;
+  time: any = 30;
+  timer: any;
+  yourAnswer: any;
 
-  results:any=[];
+  results: any = [];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.questions=this.navParams.get('exam1');
+    this.questions = this.navParams.get('exam1');
   }
 
-  ionViewDidLoad(){
-    this.question=this.questions[0];
+  ionViewDidLoad() {
+    this.question = this.questions[0];
     this.timego();
   }
 
@@ -51,83 +52,92 @@ export class LookPicRecogWordPage {
 
     this.timer = setInterval(() => {
       this.time--;
-      if(this.time==0){
+      if (this.time == 0) {
         clearInterval(this.timer);
-        for(let j=this.index;j<10;j++)
-        {
-          let result={
-            src:this.questions[j].picSrc,
-            yourAnswer:this.yourAnswer,
-            trueAnswer:this.questions[j].selected,
-            meaning:this.questions[j].meaning,
+        for (let j = this.index; j < 10; j++) {
+          let result = {
+            src: this.questions[j].picSrc,
+            yourAnswer: this.yourAnswer,
+            trueAnswer: this.questions[j].selected,
+            meaning: this.questions[j].meaning,
+            audioSrc: this.questions[j].audioSrc
           };
 
           this.results.push(result);
         }
-        this.over=true;
+        this.over = true;
       }
     }, 1000);
 
   }
 
-  nextQuestion(){
+  nextQuestion() {
 
-        if(this.selected==this.question.selected) {
-          this.score += 10;
-        }
-        let result={
-          src:this.question.picSrc,
-          yourAnswer:this.selected,
-          trueAnswer:this.question.selected,
-          meaning:this.question.meaning,
-        };
+    if (this.selected == this.question.selected) {
+      this.score += 10;
+    }
+    let result = {
+      src: this.question.picSrc,
+      yourAnswer: this.selected,
+      trueAnswer: this.question.selected,
+      meaning: this.question.meaning,
+      audioSrc: this.question.audioSrc
+    };
 
-        this.results.push(result);
+    this.results.push(result);
 
-        this.index++;
+    this.index++;
 
-        if(this.index<10){
-          this.question=this.questions[this.index];
-          this.selected1=false;
-          this.selected2=false;
-          this.selected3=false;
-          this.selected4=false;
-          this.yourAnswer='-';
-          this.selected='-';
-        }
-        else{
-          this.over=true;
-          clearInterval(this.timer);
-        }
+    if (this.index < 10) {
+      this.question = this.questions[this.index];
+      this.selected1 = false;
+      this.selected2 = false;
+      this.selected3 = false;
+      this.selected4 = false;
+      this.yourAnswer = '-';
+      this.selected = '-';
+    }
+    else {
+      this.over = true;
+      clearInterval(this.timer);
+    }
   }
 
-  select1(){
-    this.selected1=true;
-    this.selected2=false;
-    this.selected3=false;
-    this.selected4=false;
-    this.selected=this.question.option1;
+  select1() {
+    this.selected1 = true;
+    this.selected2 = false;
+    this.selected3 = false;
+    this.selected4 = false;
+    this.selected = this.question.option1;
   }
-  select2(){
-    this.selected1=false;
-    this.selected2=true;
-    this.selected3=false;
-    this.selected4=false;
-    this.selected=this.question.option2;
+  select2() {
+    this.selected1 = false;
+    this.selected2 = true;
+    this.selected3 = false;
+    this.selected4 = false;
+    this.selected = this.question.option2;
   }
-  select3(){
-    this.selected1=false;
-    this.selected2=false;
-    this.selected3=true;
-    this.selected4=false;
-    this.selected=this.question.option3;
+  select3() {
+    this.selected1 = false;
+    this.selected2 = false;
+    this.selected3 = true;
+    this.selected4 = false;
+    this.selected = this.question.option3;
   }
-  select4(){
-    this.selected1=false;
-    this.selected2=false;
-    this.selected3=false;
-    this.selected4=true;
-    this.selected=this.question.option4;
+  select4() {
+    this.selected1 = false;
+    this.selected2 = false;
+    this.selected3 = false;
+    this.selected4 = true;
+    this.selected = this.question.option4;
   }
 
+  playAudio(i) {
+    (document.getElementsByClassName("resultText")[i] as HTMLElement).style.backgroundColor = "#ff6b00";
+    setTimeout(() => {
+      (document.getElementsByClassName("resultText")[i] as HTMLElement).style.backgroundColor = "#fff";
+    }, 1000);
+    let audio = document.getElementsByClassName("word-audio")[i] as HTMLAudioElement;
+    audio.play();
+  }
 }
--- src/pages/word/word.ts
+++ src/pages/word/word.ts
@@ -15,70 +15,101 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class WordPage {
 
-  index:any;
-  words:any=[];
-  word:any={
-    word:'',
-    meaning:'',
-    picSrc:''
+  index: any;
+  words: any = [];
+  word: any = {
+    word: '',
+    meaning: '',
+    picSrc: '',
+    audioSrc: ''
   };
-  canSee=true;
+  canSee = true;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
 
   }
 
+  ionViewDidEnter() {
+    let audio = document.getElementById("word-audio") as HTMLAudioElement;
+    audio.play();
+  }
+
   ionViewWillEnter() {
-    this.words=this.navParams.get('words');
-    this.index=this.navParams.get('index');
-    this.word=this.words[this.index];
-    if(this.index===0){
-      document.getElementById('icon1').style.color='#a9a9a9';
-      document.getElementById('icon2').style.color='#427feb';
+    this.words = this.navParams.get('words');
+    this.index = this.navParams.get('index');
+    this.word = this.words[this.index];
+    if (this.index === 0) {
+      document.getElementById('icon1').style.color = '#a9a9a9';
+      document.getElementById('icon2').style.color = '#427feb';
     }
-    else if(this.index===this.words.length-1){
-      document.getElementById('icon2').style.color='#a9a9a9';
-      document.getElementById('icon1').style.color='#427feb';
-    }
-  }
-
-  showLast(){
-      if(this.index>0){
-        this.index=this.index-1;
-        this.word=this.words[this.index];
-      }
-      if(this.index===0){
-        document.getElementById('icon1').style.color='#a9a9a9';
-        document.getElementById('icon2').style.color='#427feb';
-      }
-      else{
-        document.getElementById('icon2').style.color='#427feb';
-        document.getElementById('icon1').style.color='#427feb';
-      }
-  }
-
-  showNext(){
-    if(this.index<this.words.length-1){
-      this.index=this.index+1;
-      this.word=this.words[this.index];
-    }
-    if(this.index===this.words.length-1){
-      document.getElementById('icon2').style.color='#a9a9a9';
-      document.getElementById('icon1').style.color='#427feb';
-    }
-    else{
-      document.getElementById('icon2').style.color='#427feb';
-      document.getElementById('icon1').style.color='#427feb';
+    else if (this.index === this.words.length - 1) {
+      document.getElementById('icon2').style.color = '#a9a9a9';
+      document.getElementById('icon1').style.color = '#427feb';
     }
   }
 
-  cantsee(){
-      this.canSee=false;
-      document.getElementById('englishWord').style.visibility='hidden';
-      document.getElementById('soundmark').style.visibility='hidden';
+  showLast() {
+    if (this.index > 0) {
+      this.index = this.index - 1;
+      this.word = this.words[this.index];
+    }
+    if (this.index === 0) {
+      document.getElementById('icon1').style.color = '#a9a9a9';
+      document.getElementById('icon2').style.color = '#427feb';
+    }
+    else {
+      document.getElementById('icon2').style.color = '#427feb';
+      document.getElementById('icon1').style.color = '#427feb';
+    }
+    document.getElementById("icon1").style.visibility="hidden";
+    setTimeout(()=>{
+      document.getElementById("icon1").style.visibility="visible";
+    },100);
+    setTimeout(()=>{
+      let audio = document.getElementById("word-audio") as HTMLAudioElement;
+      audio.play();
+    },500);
   }
-  cansee(){
-      this.canSee=true;
-      document.getElementById('englishWord').style.visibility='visible';
-      document.getElementById('soundmark').style.visibility='visible';
+
+  showNext() {
+    if (this.index < this.words.length - 1) {
+      this.index = this.index + 1;
+      this.word = this.words[this.index];
+    }
+    if (this.index === this.words.length - 1) {
+      document.getElementById('icon2').style.color = '#a9a9a9';
+      document.getElementById('icon1').style.color = '#427feb';
+    }
+    else {
+      document.getElementById('icon2').style.color = '#427feb';
+      document.getElementById('icon1').style.color = '#427feb';
+    }
+    document.getElementById("icon2").style.visibility="hidden";
+    setTimeout(()=>{
+      document.getElementById("icon2").style.visibility="visible";
+    },100);
+    setTimeout(()=>{
+      let audio = document.getElementById("word-audio") as HTMLAudioElement;
+      audio.play();
+    },500);
+  }
+
+  cantsee() {
+    this.canSee = false;
+    document.getElementById('englishWord').style.visibility = 'hidden';
+    document.getElementById('soundmark').style.visibility = 'hidden';
+  }
+  cansee() {
+    this.canSee = true;
+    document.getElementById('englishWord').style.visibility = 'visible';
+    document.getElementById('soundmark').style.visibility = 'visible';
+  }
+
+  playAudio() {
+    document.getElementById("play").style.visibility="hidden";
+    setTimeout(()=>{
+      document.getElementById("play").style.visibility="visible";
+    },100);
+    let audio = document.getElementById("word-audio") as HTMLAudioElement;
+    audio.play();
   }
 }
