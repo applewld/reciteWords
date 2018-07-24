f1d1d2c5b0ece6b18185a3da981e656d08bc4b77
wanglindong <158338402@qq.com>
2018-07-24 10:16:04 +0800

    完成母语外语测试页面

--- src/pages/monther-lan-foreign-lan/monther-lan-foreign-lan.ts
+++ src/pages/monther-lan-foreign-lan/monther-lan-foreign-lan.ts
@@ -15,11 +15,118 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MontherLanForeignLanPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  questions: any = [];
+  over: boolean = false;
+  score: any = 0;
+  question: any = {
+    word: '',
+    option1: '',
+    option2: '',
+    option3: '',
+    option4: '',
+    selected: ''
+  };
+
+  selected1:boolean=false;
+  selected2:boolean=false;
+  selected3:boolean=false;
+  selected4:boolean=false;
+
+  index: any = 0;
+  time: any = 30;
+  timer: any;
+  yourAnswer: any='-';
+
+  results: any = [];
+
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams) {
+    this.questions = this.navParams.get('exam6');
+    console.log(this.questions);
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad MontherLanForeignLanPage');
+    console.log('ionViewDidLoad ForeignLanMotherLanPage');
+    this.question=this.questions[0];
+    this.timego();
   }
 
+  timego() {
+    this.timer = setInterval(() => {
+      this.time--;
+      if (this.time == 0) {
+        clearInterval(this.timer);
+        for (let j = this.index; j < 10; j++) {
+          let result = {
+            word: this.questions[j].word,
+            yourAnswer: this.yourAnswer,
+            selected: this.questions[j].selected
+          };
+
+          this.results.push(result);
+        }
+        this.over = true;
+        console.log(this.results);
+      }
+    }, 1000);
+  }
+
+  select1() {
+    this.selected1=true;
+    this.selected2=false;
+    this.selected3=false;
+    this.selected4=false;
+    this.yourAnswer = this.question.option1;
+  }
+  select2() {
+    this.selected1=false;
+    this.selected2=true;
+    this.selected3=false;
+    this.selected4=false;
+    this.yourAnswer = this.question.option2;
+  }
+  select3() {
+    this.selected1=false;
+    this.selected2=false;
+    this.selected3=true;
+    this.selected4=false;
+    this.yourAnswer = this.question.option3;
+  }
+  select4() {
+    this.selected1=false;
+    this.selected2=false;
+    this.selected3=false;
+    this.selected4=true;
+    this.yourAnswer = this.question.option4;
+  }
+
+  nextQuestion() {
+
+    if (this.yourAnswer == this.question.selected) {
+      this.score += 10;
+    }
+    let result = {
+      word: this.question.word,
+      yourAnswer: this.yourAnswer,
+      selected: this.question.selected
+    };
+
+    this.results.push(result);
+
+    this.index++;
+
+    if (this.index < 10) {
+      this.question = this.questions[this.index];
+      this.selected1=false;
+      this.selected2=false;
+      this.selected3=false;
+      this.selected4=false;
+      this.yourAnswer = '-';
+    }
+    else {
+      this.over = true;
+      clearInterval(this.timer);
+      console.log(this.results);
+    }
+  }
 }
